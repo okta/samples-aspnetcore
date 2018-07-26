2e692caae99f2d2c926a25e20ce08cc9e79d17ed
Laura Rodriguez <rdz.maria.laura@gmail.com>
2018-07-26 16:58:17 -0400

    Add Okta hosted login page sample

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# samples-aspnetcore
-samples-aspnetcore
+# ASP.NET Core MVC OAuth Sample Applications for Okta
+
+This repository contains several sample applications that show you how to integrate various Okta use-cases into your ASP.NET MVC applications.
+
+Please find the sample that fits your use-case from the table below.
+
+| Sample | Description | Use-Case |
+|--------|-------------|----------|
+| [Okta-Hosted Login](/okta-hosted-login) | An application server that uses the hosted login page on your Okta org, then creates a cookie session for the user in the ASP.NET Core MVC application. | Traditional web applications with server-side rendered pages. |
+| [Resource Server](/resource-server) | This is a sample API resource server that shows you how to authenticate requests with access tokens that have been issued by Okta. | Single-Page applications. |
---
+++ okta-hosted-login/okta-aspnetcore-mvc-example/okta-aspnetcore-mvc-example/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using okta_aspnetcore_mvc_example.Models;
+
+namespace okta_aspnetcore_mvc_example.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        [Authorize]
+        public IActionResult Profile()
+        {
+            return View(HttpContext.User.Claims);
+        }
+    }
+}
