9a00a1e3be1a5b5f7808413a49d314fcf0d1f6ea
Laura Rodriguez <rdz.maria.laura@gmail.com>
2019-12-20 15:07:32 -0300

    * Recreate okta-hosted login sample using .NET Core 3.1 and Okta.AspNetCore 3.1. * Add Cake files to build the samples.

---
+++ ASP.NET Core 3.x/okta-hosted-login/okta-aspnetcore-mvc-example/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using okta_aspnetcore_mvc_example.Models;
+
+namespace okta_aspnetcore_mvc_example.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Profile()
+        {
+            return View(HttpContext.User.Claims);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
