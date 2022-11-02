c1270d0bf8eaa7983e6ebeeb228269a6ae483a5c
Bryan Apellanes <bryan.apellanes@okta.com>
2022-11-02 10:00:22 -0500

    OKTA-488266: add self hosted net6 sample

---
+++ samples-aspnetcore-6x/self-hosted-login/okta-aspnetcore-mvc-example/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace okta_aspnetcore_mvc_example.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
